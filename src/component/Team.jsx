df5a21bca931461ee03bdcca5c6650c9063e8561
udamadu11 <madu12dara@gmail.com>
2020-10-18 13:06:38 +0530

    reactwithBootstrap

---
+++ src/component/Team.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+export default ({TeamLinks}) => {
+
+    return(
+        <section className="page-section bg-light" id="team">
+            <div className="container">
+                <div className="text-center">
+                    <h2 className="section-heading text-uppercase">Our Amazing Team</h2>
+                    <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                </div>
+                <div className="row">
+                    {TeamLinks && TeamLinks.map(({name,position,img},index)=>
+                        <div className="col-lg-4">
+                        <div className="team-member">
+                            <img className="mx-auto rounded-circle" src={img} alt="" />
+                            <h4>{name}</h4>
+                            <p className="text-muted">{position}</p>
+                            <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-twitter"></i></a>
+                            <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-facebook-f"></i></a>
+                            <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                    )
+                    }
+                </div>
+                <div className="row">
+                    <div className="col-lg-8 mx-auto text-center"><p className="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p></div>
+                </div>
+            </div>
+        </section>
+    )
+
+}
---
+++ src/component/portpolio.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+export default ({portpolioLiks}) => {
+    return(
+        <section class="page-section bg-light" id="portfolio">
+        <div className="container">
+                <div className="text-center">
+                    <h2 className="section-heading text-uppercase">Portfolio</h2>
+                    <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                </div>
+                <div className="row">
+                    {portpolioLiks && portpolioLiks.map(({item,title,img} , index)=>
+                        <div className="col-lg-4 col-sm-6 mb-4">
+                        <div className="portfolio-item">
+                            <a className="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+                                <div className="portfolio-hover">
+                                    <div className="portfolio-hover-content"><i className="fas fa-plus fa-3x"></i></div>
+                                </div>
+                                <img className="img-fluid" src={img} alt="" />
+                            </a>
+                            <div className="portfolio-caption">
+                    <div className="portfolio-caption-heading">{item}</div>
+                    <div className="portfolio-caption-subheading text-muted">{title}</div>
+                            </div>
+                        </div>
+                    </div>
+                    )
+                    }
+                    
+                </div>
+            </div>
+            </section>
+    )
+}
